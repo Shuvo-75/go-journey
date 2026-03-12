f58e1909359cad52fef657bb605607f0e5efe32e
Shuvo <smrakibulhasanshuvo@gmail.com>
2026-03-13 02:24:54 +0600

    upto lec 10

--- go-project/first-program/main.go
+++ go-project/first-program/main.go
@@ -2,7 +2,76 @@ package main
 
 import "fmt"
 
+func add(num1 int, num2 int) {
+	sum := num1 + num2
+
+	fmt.Println(sum)
+}
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("What do you do for living ?")
+	// fmt.Println("Hello World!")
+
+	// data types
+	/*
+		int
+		float32
+		bool
+		string
+	*/
+
+	// variable declaration
+	/*
+		var a int = 10
+		var a = 10
+		a := 10
+		a := "Hello World!"
+		a := true
+		const a = 100
+		fmt.Println(a)
+	*/
+
+	// conditional statement
+	// > , >= , < , <= , ==
+
+	/*
+		age := 18
+
+		if age > 18 {
+			fmt.Println("You are eligible to be married")
+		} else if age < 18 {
+			fmt.Println("You are not eligible to to married")
+		} else {
+			fmt.Println("You are just a teenager, not eligible to be married")
+		}
+	*/
+
+	// and => && or => || not => !
+	/*
+		age := 18
+		sex := "male"
+		age = 20
+		if age == 20 && sex == "male" {
+			fmt.Println("You are to be married")
+		}
+	*/
+
+	// switch case
+	/*
+		a := 6
+		switch a {
+		case 1:
+			fmt.Println("a is less than 2")
+		case 2:
+			fmt.Println("a is 2")
+		default:
+			fmt.Println("a is greater than 2")
+		}
+	*/
+
+	a := 10
+	b := 20
+
+	// sum := a + b
+
+	// fmt.Println(sum)
+	add(a, b)
 }
